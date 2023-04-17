7e67254cdf03602e719402a1d2df0c4653461bb3
Ambuj <ambuj903@gmail.com>
2023-04-17 10:39:34 +0530

    Async function for handling liquidity queries

--- sample_usage.py
+++ sample_usage.py
@@ -17,13 +17,66 @@ async def sample_query(channel):
     liquidity = LiquidityQueries(channel)
     params = await liquidity.get_generic_params(1)
     print(params.to_dict())
+    
+    pools = await liquidity.get_pools(1,'true',1)
+    print(pools.to_dict())
+    
+    pool = await liquidity.get_pool(1,1)
+    print(pool.to_dict())
 
-    bank = BankQueries(channel)
-    params = await bank.get_params()
-    print(params.to_dict())
+    pair = await liquidity.get_pool(1,1)
+    print(pair.to_dict())
+
+    pairs = await liquidity.get_pools([],1)
+    print(pairs.to_dict())
+
+    deposit_request = await liquidity.get_deposit_request(1,1,1)
+    print(deposit_request.to_dict())
+
+    deposit_requests = await liquidity.get_deposit_requests(1,1)
+    print(deposit_requests.to_dict())
+    
+    withdraw_request = await liquidity.get_withdraw_request(1,1,1)
+    print(withdraw_request.to_dict())
+
+    withdraw_requests = await liquidity.get_withdraw_requests(1,1)
+    print(withdraw_requests.to_dict())
+   
+    order = await liquidity.get_order(1,1,1)
+    print(order.to_dict())
+
+    orders = await liquidity.get_orders(1,1)
+    print(orders.to_dict())
+
+    orderbooks = await liquidity.get_orderbooks(1,[],[],1)
+    print(orderbooks.to_dict())
+
+    farmer = await liquidity.get_farmer(1,1,"")
+    print(farmer.to_dict())
+
+    deserialize_pool_coin = await liquidity.get_deserialize_pool_coin(1,1,1)
+    print(deserialize_pool_coin.to_dict())
+
+    pool_incentive = await liquidity.get_pool_incentives(1)
+    print(pool_incentive.to_dict())    
+
+    farmed_pool_coin = await liquidity.get_farmed_pool_coin(1,1)
+    print(farmed_pool_coin.to_dict())  
+
+    total_active_queued_pool_coin = await liquidity.get_total_active_queued_pool_coin(1)
+    print(total_active_queued_pool_coin.to_dict()) 
+
+    orders_by_orderer = await liquidity.get_orders_by_orderer("",1,1)
+    print(orders_by_orderer.to_dict())
+
+    
+
+  #  bank = BankQueries(channel)
+   # params = await bank.get_params()
+   # print(params.to_dict())
 
 async def sample_tx(connection):
-    wallet = Wallet.from_mnemonic("seeds here")
+    wallet = Wallet.from_mnemonic("home plate head toast deputy silent piece skate truth hold certain own")
     sender = wallet.get_address().to_acc_bech32()
     msg_send = MsgSend(
         from_address=sender,
@@ -37,7 +90,7 @@ async def sample_tx(connection):
 async def main():
     connection = await get_connection()
     await sample_query(connection.channel())
-    await sample_tx(connection)
+    # await sample_tx(connection)
     connection.close()
 
 
