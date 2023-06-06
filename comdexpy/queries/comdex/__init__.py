d943968ac1844f315fc1f0d8c55d6705e9219eeb
Adeve903 <95744036+Adeve903@users.noreply.github.com>
2023-06-06 13:45:40 +0530

    Update __init__.py for queries

--- comdexpy/queries/comdex/__init__.py
+++ comdexpy/queries/comdex/__init__.py
@@ -1,1 +1,8 @@
 from comdexpy.queries.comdex.liquidity import Query as LiquidityQueries
+from comdexpy.queries.comdex.asset import Query as AssetQueries
+from comdexpy.queries.comdex.auction import Query as AuctionQueries
+from comdexpy.queries.comdex.collector import Query as CollectorQueries
+from comdexpy.queries.comdex.esm import Query as ESMQueries
+from comdexpy.queries.comdex.lend import Query as LendQueries
+from comdexpy.queries.comdex.liquidation import Query as LiquidationQueries
+from comdexpy.queries.comdex.vault import Query as VaultQueries
